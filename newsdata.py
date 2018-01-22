72b93fdf0d23d0ac4cc09336501ea4b7863a573c
Spurthi Hegde <spurthi.sh@gmail.com>
2018-01-22 23:37:05 +0400

    Making pep8 & pep257 compliant

--- newsdata.py
+++ newsdata.py
@@ -1,6 +1,12 @@
 #! /usr/bin/python3
 
-import psycopg2
+"""Newsdata module -Log Analysis with PostgreSQL.
+
+The query's are listed in the form of Query1, Query2, Query3 for
+Question1, Question2, Question3 respectively & executed.
+"""
+
+import psycopg2  # import for postgresql
 # Name of the DB to connect to is news. Followed by Querys executed on the DB.
 DBNAME = "news"
 
@@ -9,27 +15,33 @@ Question2 = "\n2. Who are the most popular article authors of all time?\n"
 Question3 = "\n3. On which days did more than 1% of requests lead to errors?\n"
 
 # ArticleLog View creation query
-''' create view artice_log_view as select count(*) as views, articles.title,
-articles.author from log,articles where log.path like concat('%',articles.slug)
-and status like '%200%' group by articles.title, articles.author order by
-views desc;'''
+view1 = """CREATE VIEW artice_log_view AS SELECT count(*) AS views, articles.title,
+articles.author FROM log,articles WHERE log.path LIKE concat('%',articles.slug)
+AND status LIKE '%200%' GROUP BY articles.title, articles.author ORDER BY
+views DESC;"""
 
-Query1 = "select title, views from artice_log_view limit 3"
-Query2 = """select authors.name, sum(artice_log_view.views) as popular_author_views
-from authors,artice_log_view where authors.id = artice_log_view.author group by
-authors.name order by popular_author_views desc"""
+Query1 = "SELECT title, views FROM artice_log_view LIMIT 3"
+Query2 = """SELECT authors.name, SUM(artice_log_view.views) AS popular_author_views
+        FROM authors,artice_log_view WHERE authors.id = artice_log_view.author
+        GROUP BY authors.name ORDER BY popular_author_views DESC"""
 
 # ErrorPercentage Log view creation query
-'''create view ErrorPercentage_log_view as select date(time) as day,round
-(100.00 * sum(case when log.status = '200 OK' then 0 else 1 end)/
-count(log.status),2) as Error Percentage from log group by day order by
-ErrorPercentage desc;'''
+view2 = """CREATE VIEW ErrorPercentage_log_view AS SELECT date(time) AS day,round
+(100.00 * sum(CASE WHEN log.status = '200 OK' THEN 0 ELSE 1 END)/
+count(log.status),2) AS Error Percentage FROM log GROUP BY day ORDER BY
+ErrorPercentage DESC;"""
 
-Query3 = """select to_char(day, 'DD Mon, YYYY'), errorpercentage from
-ErrorPercentage_log_view where errorpercentage > 1"""
+Query3 = """SELECT to_char(day, 'DD Mon, YYYY'), errorpercentage FROM
+        ErrorPercentage_log_view WHERE errorpercentage > 1"""
 
 
 def exec_results(query):
+    """Connect to the DB, execute the query & return the results.
+
+    Keyword arguement:
+    query -> String with PostgreSQL
+    returns a list.
+    """
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute(query)
@@ -39,6 +51,12 @@ def exec_results(query):
 
 
 def display_results_for_views(result):
+    """Parse & print the records in the result for Query1 & Query2.
+
+    Keyword arguement:
+    result -> Rows of List with nested lists.
+    print the desired result
+    """
     i = 0
     for r in result:
         print('\t'+str(result[i][0]) + ' ---> '+str(result[i][1])+' views')
@@ -46,6 +64,12 @@ def display_results_for_views(result):
 
 
 def display_results_for_errors(result):
+    """Parse & print the records in the result for Query3.
+
+    Keyword arguement:
+    result -> Rows of List with nested lists.
+    print the desired result
+    """
     i = 0
     for r in result:
         print('\t'+str(result[i][0])+' ---> '+str(result[i][1])+' %\n')
@@ -53,6 +77,7 @@ def display_results_for_errors(result):
 
 
 print(Question1)
+print("\nView created for Question 1 & 2 is: %s\n" % view1)
 result1 = exec_results(Query1)
 display_results_for_views(result1)
 
@@ -61,5 +86,6 @@ result2 = exec_results(Query2)
 display_results_for_views(result2)
 
 print(Question3)
+print("\nView created for Question 3 is: %s\n" % view2)
 result3 = exec_results(Query3)
 display_results_for_errors(result3)
